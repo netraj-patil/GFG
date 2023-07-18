66b21779925018db6df8661672ae74345c61674c
Netraj Patil <netrajpatil12mati@gmail.com>
2023-07-18 17:08:29 +0530

    POTD Longest repeat subsequence

---
+++ problem of day/longRepeatSubst.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		     int n=str.size();
+            int dp[n+1][n+1];
+            for(int i=n;i>-1;i--){
+                for(int j=n;j>-1;j--){
+                    if(i==n || j==n){
+                        dp[i][j]=0;
+                    }
+                    else{
+                        if(str[i]==str[j] && i!=j){
+                            dp[i][j]=dp[i+1][j+1]+1;
+                        }
+                        else{
+                            dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                      }
+                  }
+              }
+          }
+       return dp[0][0];  
+
+
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.LongestRepeatingSubsequence(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
