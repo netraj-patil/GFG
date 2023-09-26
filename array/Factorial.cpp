a7ee58fee8def9f1ce639492bd6db8290505b8f9
Netraj Patil <netrajpatil12mati@gmail.com>
2023-09-26 19:09:47 +0530

    Factorial of large no

---
+++ array/Factorial.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+// #include <bits/stdc++.h>
+#include<iostream>
+#include<conio.h>
+#include<vector>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    void multiply(vector<int> &ans, int x){
+        int carry = 0;
+        
+        for(int i=0; i<ans.size(); i++){
+            int prod = (ans[i]*x)+carry;
+            ans[i]= prod%10;
+            carry = prod/10;
+        }
+        
+        while(carry){
+            ans.push_back(carry%10);
+            carry /= 10;
+        }
+    }
+    vector<int> factorial(int N){
+        // code here
+        vector<int> ans;
+        
+        ans.push_back(1);
+        
+        for(int i= 2; i<=N; i++){
+            multiply(ans,i);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        vector<int> result = ob.factorial(N);
+        for (int i = 0; i < result.size(); ++i){
+            cout<< result[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
